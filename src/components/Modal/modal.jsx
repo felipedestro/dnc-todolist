5c5b1569df9aa72c0418955d85a84eaf16476061
Felipe Destro <felipe.s.destro@gmail.com>
2024-01-27 12:42:49 -0400

    adicionando efeito ao chamar o modal

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./modal.scss";
 
 function Modal({ children }) {
 	return (
-		<div className="modal">
+		<div className="modal modal-in">
 			<div className="modal__container">{children}</div>
 		</div>
 	);
